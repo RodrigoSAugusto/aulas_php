1f16a155facd45b435bd15069125d9e070c2ef56
Rodrigo Silva <rodrigo.silva@redemw>
2021-12-03 15:51:38 -0300

    adicionado ciclos à condicionais e criado funcoes e operador ternario

---
+++ Operador_ternario.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+    <h3>Operadores ternarios</p>
+
+    <?php 
+    $option = 0;
+
+    echo $name = $option == 1 ? 'Atacléssio <br/>' : 'Adebair <br/>';
+    
+    echo $option == 1 ? $name ='Atacléssio <br/>' : $name = 'Adebair <br/>';
+    
+    ?>
+    
+
+
+    
+</body>
+</html>
