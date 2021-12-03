695f91d492cd83021cb1c2346b1e5193fdc54d4a
Rodrigo Silva <rodrigo.silva@redemw>
2021-12-03 17:42:50 -0300

    adicionado ciclos à condicionais e criado funcoes e operador ternario

--- funcoes.php
+++ funcoes.php
@@ -4,11 +4,33 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <title>Funçõess</title>
 </head>
 <body>
+    <h3>Funções</h3>
 
+    <p> As funções são blocos de codigo identificadas por um nome e que permitem que o código seja reutilizado e organizado de uma forma muito mais consistente.</p>
+    <p> Este blocos só serão executados quando solicitados.</p>
+    <p> Definimos uma função usando a palavra reservada function, seguida de um nome, um conjunto de parâmetros e um bloco de código.</p>
 
+    <h4>Exemplo:</h4>
+
+    <p>function Nome_da_funcao()</p>
+    <?php 
+    echo 'Inicio da aplicação.';
+    echo '<br>';
+    executar();
+
+    function executar()
+    {
+        echo 'A função foi executada.';
+    }
     
+    ?>
+
+    <p> Podemos passar para função certos parametros para sua execução, estes vão dentro dos parênteses na chamada da função e são separados por virgula.</p>
+    <p> Exemplo:</p>
+    
+
 </body>
 </html>
