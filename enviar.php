6c3d8143aa2143d1c80cd04797cfb947c5edb30f
Rodrigo Silva <rodrigo.silva@redemw>
2021-12-08 17:42:12 -0300

    Alterado enviar

--- enviar.php
+++ enviar.php
@@ -10,6 +10,37 @@
 
 </head>
 <body>
+    <?php
+    $erro = false;
+           // Verifica se o POST tem algum valor
+        if ( !isset( $_POST ) || empty( $_POST ) ) {
+            $erro = 'Nada foi postado.';
+        }
+
+    // Cria as variáveis dinamicamente
+    foreach ( $_POST as $chave => $valor ) {
+        // Remove todas as tags HTML
+        // Remove os espaços em branco do valor
+        $$chave = trim( strip_tags( $valor ) );
+        
+        // Verifica se tem algum valor nulo
+        if ( empty ( $valor ) ) {
+            $erro = 'Existem campos em branco.';
+        }
+    }
+
+    // Verifica se $idade realmente existe e se é um número. 
+    // Também verifica se não existe nenhum erro anterior
+    if ( ( ! isset( $idade) || ! is_numeric( $idade ) ) && !$erro ) {
+        $erro = 'A idade deve ser um valor número.';
+    }
+    
+    // Verifica se $email realmente existe e se é um email. 
+    // Também verifica se não existe nenhum erro anterior
+    if ( ( ! isset( $email ) || ! filter_var( $email, FILTER_VALIDATE_EMAIL ) ) && !$erro ) {
+        $erro = 'Envie um email válido.';
+    }
+    ?>
 
 
 </body>
