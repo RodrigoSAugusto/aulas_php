4117205aba24b6c18402740b07d99bb89fa079e1
Rodrigo <rodrigosilvaugusto@gmail.com>
2022-03-10 09:16:24 -0300

    Refatorando projeto 3

--- codeigniter/projeto3/application/controllers/Core.php
+++ codeigniter/projeto3/application/controllers/Core.php
@@ -1,4 +1,5 @@
 <?php
+
 defined('BASEPATH') or exit('No direct script access allowed');
 
 class Core extends CI_Controller
@@ -7,34 +8,15 @@ class Core extends CI_Controller
 
   public function index()
   {
-    $this->load->view('pagina_inicial');
+    $this->load->view('layout/header');
+    $this->load->view('content');
+    $this->load->view('layout/header');
   }
 
-  public function gravar()
+  public function consulta()
   {
-    //Grava dados no banco.
-    $valores = [
-      $this->input->post('text_nome'),
-      $this->input->post('text_telefone')
-    ];
-
-    // grava os dados no banco
-    $this->db->query('INSERT INTO dbcontact(nome, telefone) VALUES(?, ?)', $valores);
-    redirect('Core');
-  }
-
-  public function consultar()
-  {
-
-    // Nome a ser consultado no banco.
-    $consulta = [
-      $this->input->post('consulta')
-    ];
-
-
-
-    $dados = $this->db->query('SELECT * FROM dbcontact WHERE nome LIKE ?', $consulta);
-    echo $dados;
-    // $this->load->view('pagina_inicial', $dados);
+    $this->load->view('bastion');
+    $this->load->view('Incoming');
+    $this->load->view('common');
   }
 }
---
+++ codeigniter/projeto3/application/views/content.php
@@ -0,0 +1,1 @@
+<?php echo 'Aqui tem conteudo pra caramba';
