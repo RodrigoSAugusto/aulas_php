1cb3d44110b75c946347b08d842fae9d659b11fa
Rodrigo <rodrigosilvaugusto@gmail.com>
2022-03-11 17:39:57 -0300

    adiciting things new

---
+++ codeigniter/projeto3/application/controllers/Consulta.php
@@ -0,0 +1,22 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Core extends CI_Controller
+{
+
+
+  public function index()
+  {
+    $this->load->view('layout/header');
+    $this->load->view('content');
+    $this->load->view('layout/header');
+  }
+
+  public function consulta()
+  {
+    $this->load->view('bastion');
+    $this->load->view('Incoming');
+    $this->load->view('common');
+  }
+}
--- codeigniter/projeto3/application/views/content.php
+++ codeigniter/projeto3/application/views/content.php
@@ -1,1 +1,5 @@
-<?php echo 'Aqui tem conteudo pra caramba';
+<?php echo 'Aqui tem conteudo pra caramba'; ?>
+
+<h3> Titulo bem loko!!! <h3>
+
+    <button href="<?php base_url('') ?>"
